94ee71e3fa12fa7c919e165ced7d8d80858a0f24
GWeerasooriya <gweerasooriya34@gmail.com>
2024-11-30 13:47:45 +0530

    change1

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -10,10 +10,11 @@ const Landing = () => {
     </nav>
     <dev classname="container page">
         <dev classname="info">
-            <h1>job<span>tracking</span>app</h1>
+        <h1>Job&nbsp;<span>Tracking</span>&nbsp;App</h1>
+
             <p>I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue bottle single-origin coffee chia. 
                 Aesthetic post-ironic venmo, quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch narwhal.</p>
-                <button classname="btn btn-hero">Login/Register</button>
+         <button classname="btn btn-hero">Login/Register</button>
         </dev>
         <img src={main} alt="job hunt" classname="img main-img"/>
     </dev>
@@ -21,6 +22,55 @@ const Landing = () => {
     </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+nav{
+    width:var(--fluid-width);
+        max-width:var(--max-width);
+        margin:10 auto;
+        hight:var(--nav-height);
+        display: flex;
+        align-items:center;
+        margin-left:100px ;
+}
+.page{
+    main-hight:calc(100vh-var(--nav-height));
+    display: grid;
+    align-items:center;
+    margin-top:3rem;
+    margin-left:100px ;
+}
+h1{
+    margin-left:100px ;
+    front-weight:700;
+    span{
+        color:var(--blue-500);
+    }
+}
+p{
+    margin-left:100px ;
+    color: var(--grey-600);
+    front-weight:20px;
+}
+.main-img{
+    display: none;
+    margin-left:1000px ;
+}
+@media (min-width:992px){
+    .page{
+        grid-template-columns:1fr 1fr;
+        column-gap:3rem;
+    }
+    .main-img{
+        display :block ;
+    }
+}
+.btn btn-hero {
+  align-self: flex-end; /* Aligns the button to the right within the flex container */
+  margin-right: 100px; /* Optional: adjusts right margin for consistency */
+}
+
+`;
 
 export default Landing
 
