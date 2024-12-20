00d186d8fdaaa291796c99c04a79a4bfe7828af8
GWeerasooriya <gweerasooriya34@gmail.com>
2024-12-20 11:41:03 +0530

    zz

--- client/src/pages/Dashbord.js
+++ client/src/pages/Dashbord.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Dashbord = () => {
   return (
     <h1>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {Logo} from '../components'
+import wrapper from '../assets/wrapper/Register'
+
+const initialState = {
+  name : '',
+  email :  '',
+  password : '',
+  isMember : true,
+}
 
 const Register = () => {
+
+  const [values,setValues] = useState(initialState)
+  // Globle state and useNavigate
+
+  const handleChange = (e) =>{
+    console.log(e.target)
+  }
   return (
     <h1>
       Register
