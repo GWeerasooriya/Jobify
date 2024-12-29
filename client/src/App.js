72a8cbd323ec9b254b2f056c46e228c376ecfe1b
GWeerasooriya <gweerasooriya34@gmail.com>
2024-12-29 17:49:29 +0530

    f

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import Landing from "./pages/Landing";
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background : red;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+  margin-top: 20px;
+`
+;
 
 function App() {
   return (
     <div>
-      
+      <Button>Click Me</Button>
       <h1>JOBIFY</h1>
       <Landing />
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
+import styled from 'styled-components'
 
 const Landing = () => {
   return (
@@ -15,11 +16,46 @@ const Landing = () => {
                 quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch narwhal.</p>
             <button className='btn btn-hero'>Login/Register</button>
         </div>
-        <img src={main} alt="job hunt" className='img main-img' />
+        <img src={main} alt="job hunt" className='img main-img' /> 
       </div>
     </main>
   )
 }
+
+const Wrapper = styled.main`
+nav{
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+}
+.page{
+    main-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+}
+h1{
+    font-weight: 700;
+    spsn{
+        color: var(--clr-primary-5);
+    }
+}
+p{
+    color: var(--grey-600);
+}
+.main-img{
+    display: none;
+}
+@media (min-width: 992px) {
+    .page{
+        grid-template-columns: 1fr 1fr;
+        column-gap: 3rem;
+    }
+}
+`
 
 export default Landing
 
