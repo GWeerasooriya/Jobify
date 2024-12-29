9b385bf9feee183f1887a826af2ef3b898c8b8e9
GWeerasooriya <gweerasooriya34@gmail.com>
2024-12-29 18:13:45 +0530

    rr

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
-import styled from 'styled-components'
-
+import Wrapper from '../assets/wrappers/Testing'
 const Landing = () => {
   return (
     <main>
@@ -11,7 +10,7 @@ const Landing = () => {
       </nav>
       <div className='container page'>
         <div className='info'>
-            <h1>job <span>tracking</span>app</h1>
+            <h1>job <span>tracking</span> app</h1>
             <p>I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue bottle single-origin coffee chia. Aesthetic post-ironic venmo, 
                 quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch narwhal.</p>
             <button className='btn btn-hero'>Login/Register</button>
@@ -22,40 +21,7 @@ const Landing = () => {
   )
 }
 
-const Wrapper = styled.main`
-nav{
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-}
-.page{
-    main-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-}
-h1{
-    font-weight: 700;
-    spsn{
-        color: var(--clr-primary-5);
-    }
-}
-p{
-    color: var(--grey-600);
-}
-.main-img{
-    display: none;
-}
-@media (min-width: 992px) {
-    .page{
-        grid-template-columns: 1fr 1fr;
-        column-gap: 3rem;
-    }
-}
-`
+
 
 export default Landing
 
