471c8fd3fd281f6974ebb1c58f5a879a8c6f99a0
GWeerasooriya <gweerasooriya34@gmail.com>
2025-01-04 16:18:43 +0530

    hh

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import { DISPLAY_ALERT } from './actions'
+import { CLEAR_ALERT, DISPLAY_ALERT } from './actions'
 
 const reducer = (state, action) => {
     if(action.type === DISPLAY_ALERT) {
-        return {...state,showAlert : true, alertType : 'danger', alertText : 'Please provide all values!'}
+        return {...state,
+            showAlert : true,
+             alertType : 'danger',
+              alertText : 'Please provide all values!'
+            }
+    }
+
+    if(action.type === CLEAR_ALERT) {
+        return {...state,
+            showAlert : false,
+             alertType : '',
+              alertText : ''
+            }
     }
  throw new Error(`No such action : ${action.type}`)
 }
